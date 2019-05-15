5757876b0d46249fecd57ffde902488ede77f7d7
tristandostaler <tristandostaler11@hotmail.com>
2019-05-15 14:40:14 -0400

    Fix a few issues

--- Tools/ADSTool.py
+++ Tools/ADSTool.py
@@ -1,28 +1,28 @@
-from pyads import ADS
+from .pyads import ADS
 
 
 ## Note: Nothing here is tested yet!
-def add_data_to_stream(file_source, stream_data):
+def add_data_to_stream(file_source, stream_name, stream_data):
     handler = ADS(file_source)
-    handler.addStream(stream_data)
+    handler.add_stream_from_string(stream_name, stream_data)
 
 def list_streams_from_file(file_source):
     handler = ADS(file_source)
-    if handler.containStreams():
-        for stream in handler.getStreams()[:]:
+    if handler.has_streams():
+        for stream in handler.streams[:]:
             print(file_source + ":" + stream)
             
 def extract_streams_from_file(file_source):
     handler = ADS(file_source)
-    if handler.containStreams():
-        for stream in handler.getStreams()[:]:
+    if handler.has_streams():
+        for stream in handler.streams[:]:
             print(file_source + ":" + stream)
             with open(stream, 'wb') as f:
-                f.write(handler.getStreamContent(stream))
+                f.write(handler.get_stream_content(stream))
 
 def remove_streams_from_file(file_source):
     handler = ADS(file_source)
-    if handler.containStreams():
-        for stream in handler.getStreams()[:]:
+    if handler.has_streams():
+        for stream in handler.streams[:]:
             print(file_source + ":" + stream)
-            handler.removeStream(stream)
+            handler.streams.remove(stream)
